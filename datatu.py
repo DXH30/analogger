c14cc8ca296656583cf8ff42bcbe14c43ac954bb
DXH30 <dhs0223@gmail.com>
2020-01-29 02:29:31 +0800

    Upgrade Pekerjaan

--- datatu.py
+++ datatu.py
@@ -1,12 +1,37 @@
 import os, sys, serial
 import serial.tools.list_ports
+import lcddriver
+from time import *
 
+lcd = lcddriver.lcd()
+
+filename="datalog.txt"
+datafile = open(filename, 'ab')
+nilaike = 0
+ser = serial
 while True:
-    ser = serial.Serial(serial.tools.list_ports.comports()[2].device, 115200, timeout=1)
-    filename="datalog.txt"
-    datafile=open(filename, 'ab')
-    while True:
-        line = ser.readline()
-        datafile.write(line)
-    datafile.close()
-    ser.close()
+    try:
+        ser = serial.Serial(serial.tools.list_ports.comports()[0].device, 115200, timeout=1)
+        datafile=open(filename, 'ab')
+        while True:
+            line = ser.readline()
+            # Nampil setiap 10 data sekali
+            if ((nilaike % 1000) == 0):
+                try:
+                    lcd = lcddriver.lcd()
+                    datan = line.split(',')
+                    y1 = float(datan[0])+float(datan[1])+float(datan[2])
+                    y2 = (float(datan[0])+float(datan[2])+100)/(float(datan[1])+float(datan[1])+1)
+                    lcd.lcd_display_string("Y1 = "+str(round(y1,3)), 1)
+                    lcd.lcd_display_string("Y2 = "+str(round(y2,3)), 2)
+                except Exception as e:
+                    print(e)
+            datafile.write(line)
+            nilaike = nilaike + 1
+        datafile.close()
+        ser.close()
+    except KeyboardInterrupt:
+        print("Tutup")
+        datafile.close()
+        ser.close()
+        break;
